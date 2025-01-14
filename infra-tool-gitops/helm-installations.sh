a3d12a5a573e87b1a075933c695b115270ac416d
TAIWO LAWAL <50557587+Taiwolawal@users.noreply.github.com>
2025-01-14 09:01:52 +0000

    updated shells script for helm installation

--- infra-tool-gitops/helm-installations.sh
+++ infra-tool-gitops/helm-installations.sh
@@ -1,33 +1,75 @@
+#!/bin/bash
 
 # Kube-Prometheus-Stack
+echo "Installing Prometheus..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring --create-namespace
+helm repo update prometheus-community
+helm install prometheus \
+  prometheus-community/kube-prometheus-stack \
+  -n monitoring --create-namespace \
+  --version 26.1.0
 
 
 # AWS Load Balancer Controller
+echo "Installing AWS Load Balancer Controller..."
 helm repo add eks https://aws.github.io/eks-charts
-helm install \
-  aws-load-balancer-controller \
+helm repo update eks
+helm install aws-load-balancer-controller \
   eks/aws-load-balancer-controller \
   --set clusterName=dev-eks \
   -n kube-system \
   --set serviceMonitor.enabled=true \
   --set serviceMonitor.additionalLabels.release=prometheus \
   --set serviceAccount.create=false \
-  --set serviceAccount.name=aws-load-balancer-controller 
+  --set serviceAccount.name=aws-load-balancer-controller \
   --version 1.11.0
 
 
 # External DNS
+echo "Installing External DNS..."
 helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
-helm show values external-dns/external-dns --version 1.15.0  > external-dns.yaml
-
+helm repo update external-dns
 helm install external-dns \
-  -n external-dns
+  -n external-dns \
   external-dns/external-dns \
   --set provider=aws \
   --set serviceMonitor.enabled=true \
   --set serviceMonitor.additionalLabels.release=prometheus \
   --set serviceAccount.create=false \
-  --set serviceAccount.name=external-dns-sa 
+  --set serviceAccount.name=external-dns-sa \
   --version 1.15.0
+
+
+# KuebeArmor
+echo "Installing KuebeArmor..."
+helm repo add kubearmor https://kubearmor.github.io/charts
+helm repo update kubearmor
+helm install kubearmor \
+  -n kubearmor --create-namespace \
+   kubearmor/kubearmor \
+   --version 1.4.6
+
+
+# Metrics-Server
+echo "Installing Metrics-Server..."
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+helm repo update metrics-server
+helm install metrics-server \
+  -n metrics-server --create-namespace \
+  metrics-server/metrics-server \
+  --set serviceMonitor.enabled=true \
+  --set serviceMonitor.additionalLabels.release=prometheus \
+  --version 3.12.2
+
+
+# External Secrets
+echo "Installing External Secrets..."
+helm repo add external-secrets https://charts.external-secrets.io
+helm repo update external-secrets 
+helm install external-secrets  \
+  -n external-secrets --create-namespace \
+  external-secrets/external-secrets \
+  --set serviceMonitor.enabled=true \
+  --set serviceMonitor.additionalLabels.release=prometheus \
+  --version 0.12.1
+
